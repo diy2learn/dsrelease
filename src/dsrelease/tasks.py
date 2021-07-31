f0ee894b89b95237e693936296bf2f73c10889ee
diy2learn <bian.tquang@gmail.com>
2021-07-31 08:27:51 +0200

    get current package version

--- src/dsrelease/tasks.py
+++ src/dsrelease/tasks.py
@@ -2,10 +2,10 @@ from invoke import task
 
 
 @task
-def tag(c):
+def tag(ctx):
     """
     Tags a new git release, automates merging develop into master.
     Follows semantic versioning.
     """
-    print("it works!")
-    pass
+    current_ver = ctx.run("git describe", hide=True, warn=True).stdout
+    print("current_ver: ", current_ver)
